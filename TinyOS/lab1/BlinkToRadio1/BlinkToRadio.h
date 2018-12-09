cdaee0ec693ed69c83ab3c824d7653991d3ad111
Pan Yue <zxc479773533@gmail.com>
2018-12-09 22:54:08 +0800

    Finish TinyOS lab01

---
+++ TinyOS/lab1/BlinkToRadio1/BlinkToRadio.h
@@ -0,0 +1,24 @@
+/*
+ * HUST-IOT-Network-Labs TinyOS::lab01::BlinkToRadio1
+ * 
+ * Created by zxcpyp
+ * 
+ * Github: zxc479773533
+ */
+
+#ifndef BLINKTORADIO_H
+#define BLINKTORADIO_H
+
+enum {
+  AM_BLINKTORADIO = 6,
+  TIMER_PERIOD_MILLI = 1000,
+  NOID_ID_1 = 1,
+  NOID_ID_2 = 2
+};
+
+typedef nx_struct BlinkToRadioMsg {
+  nx_uint16_t nodeid;
+  nx_uint16_t counter;
+} BlinkToRadioMsg;
+
+#endif
